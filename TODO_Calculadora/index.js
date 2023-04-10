3ab4210113c7d224d388b9377abd0ee81aeffa4f
Ruben-R-C <Info@blogruben.com>
2023-04-10 23:23:37 +0200

    init

---
+++ TODO_Calculadora/index.js
@@ -0,0 +1,36 @@
+const pantalla = document.querySelector(".pantalla");
+const botones = document.querySelectorAll(".btn")
+
+botones.forEach(boton => {
+    boton.addEventListener("click", () => {
+        const txtBtnClick = boton.textContent;
+
+        if(boton.id === "c"){
+            pantalla.textContent = "0";
+            return;
+        }
+
+        if(boton.id === "borrar"){
+            if(pantalla.textContent.length === 1){
+                pantalla.textContent = "0";
+                return;
+            }
+            pantalla.textContent = pantalla.textContent.slice(0,-1);
+            return;
+        }
+
+        if(boton.id === "igual"){
+            pantalla.textContent = eval(pantalla.textContent);
+            return;
+        }
+
+        if(pantalla.textContent === "0"){
+            pantalla.textContent = txtBtnClick;
+            return;
+        }
+
+        pantalla.textContent += txtBtnClick;
+    })
+})
+
+
